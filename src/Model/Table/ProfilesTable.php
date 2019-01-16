c81e7e216e96f0794b917d38ba0215f769a26bb8
Miha Nahtigal <miha@malamalca.com>
2019-01-16 21:22:40 +0100

    Fix deprecated paths

--- src/Model/Table/ProfilesTable.php
+++ src/Model/Table/ProfilesTable.php
@@ -6,7 +6,7 @@ use ArrayObject;
 use Cake\Core\Configure;
 use Cake\Datasource\EntityInterface;
 use Cake\Event\Event;
-use Cake\Network\Email\Email;
+use Cake\Mailer\Email;
 use Cake\ORM\Query;
 use Cake\ORM\RulesChecker;
 use Cake\ORM\Table;
